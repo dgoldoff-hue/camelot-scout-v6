b39c1f3080c9a6c3552d3a91022d38fec39d83a9
Diamond Dave <dgoldoff@camelot.nyc>
2026-04-20 03:08:24 -0400

    Simplify main.tsx: Remove AuthProvider wrapper
    Removed HashRouter and AuthProvider from the render tree.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { AuthProvider } from './lib/AuthContext';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <AuthProvider>
-            <HashRouter>
-                    <App />
-                          </HashRouter>
-                              </AuthProvider>
-                                </React.StrictMode>
-                                );
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>React.StrictMode>,
+  )</React.StrictMode>
