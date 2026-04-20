e28a1cac35e539a4dec4f7efd6d068a6c1163486
Diamond Dave <dgoldoff@camelot.nyc>
2026-04-20 01:39:08 -0400

    Wrap App in AuthProvider and HashRouter
    Refactor the component structure to include AuthProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { AuthProvider } from './lib/AuthContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
-# 1. Navigate to your desired folder (e.g., Desktop)
-cd Desktop
-
-# 2. Clone the repo
-git clone https://github.com/dgoldoff-hue/camelot-scout-v6.git
-cd camelot-scout-v6
-
-# 3. Verify the commit
-git log --oneline -1
-
-# 4. Create the AuthContext.tsx file
-# (I'll give you the content below to save)
-
-# 5. Push to GitHub
-git push origin main
+    <React.StrictMode>
+        <AuthProvider>
+              <HashRouter>
+                      <App />
+              </HashRouter>HashRouter>
+        </AuthProvider>AuthProvider>
+    </React.StrictMode>React.StrictMode>
+  );</React.StrictMode>
